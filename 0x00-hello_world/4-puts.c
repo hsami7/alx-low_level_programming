08e1d169b4c60b6914f857dbb7507990f144cb08
Hatim <hatimsami555@gmail.com>
2023-12-14 15:28:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-	/**
- 	* main - prints the phrase "Programming is like building a multilingual puzzle
- 	* Return: 0 if exited properly, non-zero otherwise
-        */
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+/**
+* main - prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
+*/
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
