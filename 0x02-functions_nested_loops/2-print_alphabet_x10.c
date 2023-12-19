8c56a5f5043a4dd4bdfb8bdc940354bf3c23b6c8
Hatim <hatimsami555@gmail.com>
2023-12-19 13:02:00 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-        char i;
-	int count;
+	char	i;
+	int	count;
 
 	count = 0;
 	while (count < 10)
 	{
-        	i = 'a';
-        	while (i <= 'z')
-        	{
-                	_putchar(i);
-                	i++;
-        	}
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
 		count++;
 		_putchar('\n');
 	}
