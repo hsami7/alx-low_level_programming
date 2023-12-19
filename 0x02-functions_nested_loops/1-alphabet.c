6c7cf2cf3cc324cd5b029b07d1ae1fd5dbefff2d
Hatim <hatimsami555@gmail.com>
2023-12-19 12:00:58 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while(i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
