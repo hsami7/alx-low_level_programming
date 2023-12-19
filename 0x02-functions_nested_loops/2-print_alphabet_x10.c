b98833eab973d62e21e8942bdd6ec5f90ff8fd7a
Hatim <hatimsami555@gmail.com>
2023-12-19 12:52:11 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+        char i;
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+        	i = 'a';
+        	while (i <= 'z')
+        	{
+                	_putchar(i);
+                	i++;
+        	}
+		count++;
+		_putchar('\n');
+	}
+}
