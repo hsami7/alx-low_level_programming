f4c4ec491326adbd4c27d7831bab4dbc23630aa1
Hatim <hatimsami555@gmail.com>
2023-12-19 11:24:28 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
+/**
  * main - Entry point
  *
- * Return: nothing
- * */
+ * Return: nothing, return void
+ */
 
 int main(void)
 {
@@ -17,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
